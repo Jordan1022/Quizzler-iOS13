4873f3303551be2cbfdd261fe8f83ca322a85d4b
Jordan Allen <land.of.jordan@gmail.com>
2020-03-10 14:36:10 -0700

    adjusted MVC to order

---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,20 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Jordan Allen on 3/10/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+    }
+    
+}
